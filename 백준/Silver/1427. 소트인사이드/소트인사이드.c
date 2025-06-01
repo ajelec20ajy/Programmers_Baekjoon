2ebe9fe0c1d7966101aaaa0009e409d5aee69d87
AjouPenguin <121323200+ajelec20ajy@users.noreply.github.com>
2025-06-01 16:16:54 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int dec(int N, int c) {
+	if (c == 0) return N;
+}
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	int len = 1, temp = N;
+	while ((temp /= 10) != 0) len++;
+
+	int* num = (int*)malloc(sizeof(int) * len);
+
+	temp = N;
+	for (int i = len - 1; i >= 0; i--) {
+		num[i] = temp % 10;
+		temp /= 10;
+	}
+
+
+	for (int i = 9; i >= 0; i--) {
+		for (int j = 0; j < len; j++) {
+			if (i == num[j]) printf("%d", i);
+		}
+	}
+	
+
+	return 0;
+}
