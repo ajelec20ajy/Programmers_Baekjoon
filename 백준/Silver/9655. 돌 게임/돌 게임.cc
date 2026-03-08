5ed5b9462c41e57e23ffb75d39758d4a8cf638d5
AjouPenguin <121323200+ajelec20ajy@users.noreply.github.com>
2026-03-08 23:18:13 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -15,15 +16,29 @@ int main() {
     // dp[4] : 0
     // dp[5] : 1
     // dp[6] : 0
-    // dp[7] : 1
-    // dp[8] : 0
-    // dp[9] : 1
-    // 그냥 짝수면 창영이가 이기는거 아님?
-
-
+    // 그냥 짝수면 창영이가 이기는거 아님? 1하고 3밖에 없으니깐. 3은 결국 걍 자기턴으로 돌아온거자나
+    // 억지 dp 돌려볼까?
+    // dp[1] = 1, dp[2] = 0, dp[n] = dp[n-1]-dp[n-2].. n이 홀수면 -1이고, 짝수면 1이네.
+    // dp[n] = (1+dp[n-1]-dp[n-2])? 0 : 1
+    
+    vector<int> dp;
+    dp.push_back(0); // 의미없는데 그냥 인덱스맞추기위함
+    dp.push_back(1); // dp[1]
+    dp.push_back(0); // dp[2]
+    dp.push_back(1); // dp[3]
+    
     int N; cin >> N;
 
+    /*
     if (N % 2) cout << "SK";
+    else cout << "CY";
+    */
+
+    for (int i = 4; i <= N; i++) {
+        dp.push_back((1 + dp[i - 1] - dp[i - 2]) ? 0 : 1);
+    }
+
+    if (dp[N]) cout << "SK";
     else cout << "CY";
 
     return 0;
